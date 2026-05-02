015637bd116a3ef889c7712ed7a058788bd19efb
anamicuamicu <131956903+anamicuamicu@users.noreply.github.com>
2026-05-02 11:35:40 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>UnsentWords</title>
+    <style>* { margin: 0; padding: 0; box-sizing: border-box; } body { overflow: hidden; background: #03010c; }</style>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
