6e14378e720eb5ed8e8fae771dba8b345f6d2ff6
anamicuamicu <131956903+anamicuamicu@users.noreply.github.com>
2026-05-02 11:43:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>UnsentWords</title>
-    <style>* { margin: 0; padding: 0; box-sizing: border-box; } body { overflow: hidden; background: #03010c; }</style>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App, null));
